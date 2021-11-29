d12c6e8dad43474737d94ab493c2d6154284518a
yayaveli <yabadji2010@gmail.com>
2021-11-29 15:24:14 +0000

    Add Login and Signup Form

---
+++ frontend/src/components/authentication/Login.jsx
@@ -0,0 +1,83 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [show, setShow] = useState(false)
+
+  const handleChange = ({ currentTarget }) => {
+    const { name, value } = currentTarget
+    setUser((u) => ({ ...u, [name]: value }))
+  }
+
+  const handleSubmit = () => {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <VStack spacing="5px">
+        <FormControl id="email" isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input
+            name="email"
+            placeholder="Enter Your Email"
+            onChange={handleChange}
+            value={user.email}
+          />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Password</FormLabel>
+          <InputGroup>
+            <Input
+              name="password"
+              placeholder="Enter Your Password"
+              onChange={handleChange}
+              value={user.password}
+              type={show ? 'text' : 'password'}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1,75rem" size="sm" onClick={() => setShow((s) => !s)}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+        <Button
+          colorScheme="blue"
+          width="100%"
+          style={{ marginTop: 15 }}
+          type="submit"
+        >
+          Login
+        </Button>
+        <Button
+          variant="solid"
+          colorScheme="red"
+          width="100%"
+          style={{ marginTop: 15 }}
+          onClick={() =>
+            setUser({
+              email: 'guest@example.com',
+              password: '123456',
+            })
+          }
+        >
+          Get Guest User's Credentials
+        </Button>
+      </VStack>
+    </form>
+  )
+}
+
+export default Login
